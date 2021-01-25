dc7ddbdf218aabe365efb6e95aee5aa606509831
yangchaojun <yangchaojunxs@126.com>
2021-01-25 16:36:01 +0800

    新增koa-jwt

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const Koa = require('koa')
+const jwt = require('koa-jwt')
 const { router } = require('./router')
+const { TOKEN_SERECT } = require('./config')
 
 const app = new Koa()
+
+// app.use(jwt({ secret: TOKEN_SERECT }))
 
 app.use(router.routes()).use(router.allowedMethods())
 
---
+++ config/index.js
@@ -0,0 +1,5 @@
+const TOKEN_SERECT = 'it_is_serect'
+
+module.exports = {
+  TOKEN_SERECT
+}
